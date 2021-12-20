5e7f774f749bc305b3349c9e767190c01c4d7cc8
Mak~ <93770131+b1nsal1ushan@users.noreply.github.com>
2021-12-20 18:45:19 +0300

    Update PasswordGenerator.py
    This is the 4th version and it will have no Functions so that it is easy to modify and handle the code.

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,52 +1,74 @@
 import random
 
-#setting up variables
-newPassword = ""
-preiviousDecision = 0
-length = 0
-
-#setting up strings
 stringOfCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 stringOfSmall = "abcdefghijklmnopqrstuvwxyz"
 stringOfNum = "1234567890"
 stringOfSpecial = "!@#$%^&*()_+={[}]':;><?/"
 
-#main loop and generating a password
-while(True):
+def checkEveryCharacter(newPassword):
+    checkCaps = newPassword.find(stringOfCaps)
+    checkSmall = newPassword.find(stringOfSmall)
+    checkNum = newPassword.find(stringOfNum)
+    checkSpec = newPassword.find(stringOfSpecial)
 
-    #taking everycharacter randomly    
-    capCharacter = random.choice(stringOfCaps)
-    smallCharacter = random.choice(stringOfSmall)
-    Num = random.choice(stringOfNum)
-    special = random.choice(stringOfSpecial)
+    if (checkCaps == -1):
+        newPassword = newPassword + random.choice(stringOfCaps)
+    if (checkSmall == -1):
+        newPassword = newPassword + random.choice(stringOfSmall)
+    if (checkNum == -1):
+        newPassword = newPassword + random.choice(stringOfNum)
+    if (checkSpec == -1):
+        newPassword = newPassword + random.choice(stringOfSpecial)
 
-    #sub-loop of putting character and making sure it's not previously used and it contain every character
+    return newPassword
+
+
+def newPassword(length):
+    #setting up variables
+    newPassword = ""
+    preiviousDecision = 0
+    lengthOfPassword = 0
+
+    #setting up strings
+    
+
+    #main loop and generating a password
     while(True):
 
-        #taking a random from the strings
-        decision = random.randint(1, 4)
-        if (decision == 1):
-            currentCharacter = random.choice(stringOfCaps)
-        elif (decision == 2):
-            currentCharacter = random.choice(stringOfSmall)
-        elif (decision == 3):
-            currentCharacter = random.choice(stringOfNum)
-        elif (decision == 4):
-            currentCharacter = random.choice(stringOfSpecial)
+        #taking everycharacter randomly    
+        capCharacter = random.choice(stringOfCaps)
+        smallCharacter = random.choice(stringOfSmall)
+        Num = random.choice(stringOfNum)
+        special = random.choice(stringOfSpecial)
 
-        #checking if it is in the passowrd or not. Also checking it chooses a different cattegory
-        if (preiviousDecision != decision and newPassword.find(currentCharacter) == -1):
-            break
+        #sub-loop of putting character and making sure it's not previously used and it contain every character
+        while(True):
 
-    #putting characters together
-    newPassword = newPassword + currentCharacter
-    length = length + 1
-    preiviousDecision = decision
+            #taking a random from the strings
+            decision = random.randint(1, 4)
+            if (decision == 1):
+                currentCharacter = random.choice(stringOfCaps)
+            elif (decision == 2):
+                currentCharacter = random.choice(stringOfSmall)
+            elif (decision == 3):
+                currentCharacter = random.choice(stringOfNum)
+            elif (decision == 4):
+                currentCharacter = random.choice(stringOfSpecial)
 
-    #stopping if it is more than 8
-    stop = random.randint(0, 2)
-    if (length >= 8 and stop == 2):
-        break    
-    
-#New Password!!
-print(newPassword)
+            #checking if it is in the passowrd or not. Also checking it chooses a different cattegory
+            if (preiviousDecision != decision and newPassword.find(currentCharacter) == -1):
+                break
+
+        #putting characters together
+        newPassword = newPassword + currentCharacter
+        lengthOfPassword = lengthOfPassword + 1
+        preiviousDecision = decision
+
+        #stopping        
+        if (lengthOfPassword == length):
+            break    
+
+    return newPassword
+
+password = newPassword(8)
+print(checkEveryCharacter(password))
